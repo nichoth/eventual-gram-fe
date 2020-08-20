0aa7089a9b698e214a066f47d1909fc7c4a9097a
nichoth <nichoth@gmail.com>
2020-08-20 13:53:37 -0700

    example runs

--- example/index.js
+++ example/index.js
@@ -1,2 +1,13 @@
-var EventualGram = require('../src')
-EventualGram.createElement(document.getElementById('content'))
+var Ev = require('../src')
+var struct = require('observ-struct')
+
+function subscribe () {
+
+}
+
+var state = struct({})
+var { bus, view } = Ev(state)
+
+subscribe(bus, state)
+
+Ev.createElement(view, document.getElementById('content'))
--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 var React = require('react')
+var { Component, createElement } = React
+var h = createElement
 var ReactDOM = require('react-dom')
+var Bus = require('@nichoth/events')
+var xtend = require('xtend')
+// var struct = require('observ-struct')
 
-function Ev (props) {
-    return <div>example</div>
+function connect (state, View) {
+    var bus = Bus({ memo: true })
+
+    function emit () {
+        return _bus.emit.apply(_bus, arguments)
+    }
+
+    class Connector extends Component {
+        constructor(props) {
+            super(props)
+            this.state = state()
+        }
+
+        componentDidMount () {
+            var self = this
+            state(function onChange (data) {
+                self.setState(data)
+            })
+        }
+
+        render () {
+            return h(View, xtend(this.props, { emit }), this.props.children)
+        }
+    }
+
+    return { bus, view: Connector }
 }
 
-Ev.createElement = function (el) {
-    ReactDOM.render(<Ev />, el)
+function Ev (state) {
+    var view = () => (<div>example</div>)
+    var { bus, view } = connect(state, view)
+    return { bus, view }
+}
+
+Ev.createElement = function (EventualView, el) {
+    ReactDOM.render(<EventualView />, el)
 }
 
 module.exports = Ev
